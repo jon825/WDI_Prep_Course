0e6a35c72ec0a94fe1a85e8a66ebe792bb6d6394
jon kui <jon kui>
2016-09-21 17:57:01 -0400

    trouble with excercise 6 challenge 5

---
+++ Challenge_5/challenge.js
@@ -0,0 +1,21 @@
+var name = "Jonathan"
+console.log("Hello" + name)
+
+var x = 1125
+var answer = x % 2
+console.log(answer)
+
+var a = true
+var b = false
+var c = true
+console.log(a || b || c)
+
+var a = true
+var b = false
+var c = true
+console.log(a && b &&c)
+
+var answer1 = 10 - 3 * 5/4 + 202 % 3
+var answer2 = 10 - (3 * (5/4) + (202 % 3))
+console.log(answer1)
+console.log(answer2)
